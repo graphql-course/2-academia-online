b4f4efb9ec9ab6a8644c7f25ab44a1c8a010da6f
Anartz Mugika Ledo <mugan86@gmail.com>
2020-05-28 12:36:06 +0200

    Academia Online Modularize GraphQL Schemas

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,16 @@
 import 'graphql-import-node';
-import typeDefs from './schema.graphql';
+// import typeDefs from './schema.graphql';
 import { GraphQLSchema } from 'graphql';
 import { makeExecutableSchema } from 'graphql-tools';
 import resolvers from './../resolvers/resolversMap';
+
+import path from 'path';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+
+const typesArray = loadFilesSync(path.join(__dirname, './**/*.graphql'));
+
+const typeDefs = mergeTypeDefs(typesArray);
 
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
