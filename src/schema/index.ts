03c48bf369384fbdf085684da71a8b53b1c67e4c
Anartz Mugika Ledo <mugan86@gmail.com>
2020-05-28 11:25:05 +0200

    Modularize schema with @graphql-tools

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,13 @@
 import 'graphql-import-node';
-import typeDefs from './schema.graphql';
+// import typeDefs from './schema.graphql';
 import { GraphQLSchema } from 'graphql';
 import { makeExecutableSchema } from 'graphql-tools';
 import resolvers from './../resolvers/resolversMap';
+
+import { mergeTypeDefs } from '@graphql-tools/merge';
+import { loadFilesSync } from '@graphql-tools/load-files';
+ 
+const typeDefs = mergeTypeDefs(loadFilesSync(`${__dirname}/**/*.graphql`));
 
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
